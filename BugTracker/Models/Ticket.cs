3843bd55f238a2e7ec82fab44c760054ea4ffa87
joaquim6399 <joaquim6399@gmail.com>
2024-04-20 11:28:45 +0100

    Added Ticket Model and some properties
    To represent a ticket in its most basic form

---
+++ BugTracker/Models/Ticket.cs
@@ -0,0 +1,13 @@
+﻿namespace BugTracker.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
