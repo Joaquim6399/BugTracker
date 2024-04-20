0815ae8353af00cdd3c0820a1e27b3c0dc19a342
joaquim6399 <joaquim6399@gmail.com>
2024-04-20 18:00:01 +0100

    Added ApplicationDbContext and Entity Framework to project
    In order to have access to Db and perform CRUD operations

---
+++ BugTracker/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base (options)
+        {
+            
+        }
+        // public DbSet<Ticket> Tickets { get; set; }
+
+        //public DbSet<Project> Projects { get; set; }
+
+    }
+}
--- BugTracker/Models/Project.cs
+++ BugTracker/Models/Project.cs
@@ -2,5 +2,10 @@
 {
     public class Project
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Ticket> ListOfTickets { get; set; }
+
     }
 }
